e2761aca0d6a56850e43e292d25eb0186fb68f4d
RomanAbrosimov <goride@mail.ru>
2016-07-28 16:56:34 +0300

    e

--- js/script.js
+++ js/script.js
@@ -109,4 +109,21 @@ $(document).ready(function(){
   }
   }
 
+// Переключатель коттеджей
+
+  var hideItem = $('.house-desc'),
+  activeSelect = $('#flagman'),
+  firstShow = $('#house-desc__flagman');
+
+  hideItem.hide();
+  firstShow.show();
+  activeSelect.attr('checked', 'checked');
+
+  $('.cottage__choise').on('click', function(){ 
+  var forData = $(this).attr('for'),
+  showItem = '#house-desc__'+forData;
+
+  $(hideItem).hide();
+  $(showItem).show();
+  });
 });
